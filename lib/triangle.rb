e927ae1950b5767b436651d27b66b8ec2aedf704
Sharon Mun <sharonymun@gmail.com>
2018-10-18 21:04:20 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,41 @@
 class Triangle
-  # write code here
+attr_accessor :a, :b, :c
+
+  # def initialize(length_a, length_b, length_c)
+  # @triangle_length = []
+  # @triangle_length << length_a
+  # @triangle_length << length_b
+  # @triangle_length << length_c
+  # end 
+  
+  def initialize(a,b,c)
+    @a = a
+    @b = b
+    @c = c
+  end
+  
+  def kind
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || c == a
+      :isosceles
+    else
+      :scalene
+    end
+  end
 end
+
+  def true_triangle
+    true_triangle = [(a + b > c), (a + c > b), (b + c > a)] 
+    
+    [a,b,c].each { |x| actual_triangle}
+      
+      if [a,b,c] > 0 && actual_triangle
+                true
+      else    
+        false
+  end
+  end         
+class TriangleError < StandardError
+end
+
